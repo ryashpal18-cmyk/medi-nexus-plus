c043383b562fa7c81b4266feca2e389ccbaeba6a
ryashpal18-cmyk <ryashpal18@gmail.com>
2026-04-25 09:17:53 +0530

    Fixed embedded repo issue

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,16 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onNewBill: (callback) => ipcRenderer.on('new-bill', callback),
-  platform: process.platform,
-  isElectron: true
-})
+const {contextBridge,ipcRenderer}=require('electron')
+contextBridge.exposeInMainWorld('electronAPI',{
+saveBill:(b)=>ipcRenderer.invoke('save-bill',b),
+getBills:()=>ipcRenderer.invoke('get-bills'),
+savePatient:(p)=>ipcRenderer.invoke('save-patient',p),
+getPatients:()=>ipcRenderer.invoke('get-patients'),
+findPatient:(ph)=>ipcRenderer.invoke('find-patient',ph),
+searchPatients:(q)=>ipcRenderer.invoke('search-patients',q),
+setSetting:(k,v)=>ipcRenderer.invoke('set-setting',k,v),
+getSetting:(k)=>ipcRenderer.invoke('get-setting',k),
+syncNow:()=>ipcRenderer.invoke('sync-now'),
+checkInternet:()=>ipcRenderer.invoke('check-internet'),
+getDbPath:()=>ipcRenderer.invoke('get-db-path'),
+onSyncStatus:(cb)=>ipcRenderer.on('sync-status',(_,d)=>cb(d)),
+onOpenSettings:(cb)=>ipcRenderer.on('open-settings',cb),
+isElectron:true,platform:process.platform})
